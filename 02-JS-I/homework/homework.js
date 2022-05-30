803b092c2171fa02a066d7d7ae1c314cb3eceb43
Diego Martinez <103518088+diegomm990@users.noreply.github.com>
2022-05-30 19:46:04 -0300

    Tarea de JS completa

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -227,6 +227,7 @@ function esVocal(letra){
   if(letra.length > 1) return "Dato incorrecto"
   if(letra === "a" || letra === "e"|| letra === "i" || letra === "o"|| letra === "u") return "Es vocal"
   return "Dato incorrecto"
+  
 }
 
 
